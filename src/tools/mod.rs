97a6f5d1f6ce3b6f2e70cd0b72bfedb98f5f6998
Solaris5959 <connormmcdonald@gmail.com>
2026-09-25 12:45:26 -0400

    Added Basic File structure

---
+++ src/tools/mod.rs
@@ -0,0 +1,16 @@
+// ==========================================
+// MODULES
+// ==========================================
+// pub mod file_one;
+// pub mod file_two;
+
+// ==========================================
+// IMPORTS
+// ==========================================
+// use crate::models::state::AppState; 
+
+// ==========================================
+// FLATTENED IMPORTS
+// ==========================================
+// pub use file_one::SomeImportantStruct;
+// pub use file_two::{my_function, AnotherStruct};
